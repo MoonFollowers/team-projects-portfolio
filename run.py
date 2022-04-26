d69351db7a4c6b80c0d3bae3d98c113b77e2d500
migdracios <migdracios@gmail.com>
2022-04-26 11:19:30 +0900

    fix initial setting

--- run.py
+++ run.py
@@ -1,58 +1,36 @@
 import pygame
 
-
-# show grid function
-def display_grid():
-    for idx, rect in enumerate(Grid):
-        if idx == 1:
-            pygame.draw.rect(screen, WHITE, rect)
-
-
-# initial set
+# 1. 기본 초기화(반드시 해야됨)
 pygame.init()
-screen_width = 1000
-screen_height = 1000
+# 화면 크기 설정
+screen_width = 480  # 가로
+screen_height = 640  # 세로
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Battle Chess")
-pygame.font.Font(None, 40)
-
-# var def
-## bgm
-pygame.mixer.init()
-pygame.mixer.music.load('bgm.mp3')
-pygame.mixer.music.play(-1)
-## FPS
+# 화면 타이틀
+pygame.display.set_caption("Title")
+# Frame Per Second
 clock = pygame.time.Clock()
 
-## Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-## Location
-x = 0
-y = 0
-## Size
-Cell = 100
-Square = pygame.Rect(0,0,100,100)
-## Grid
-row = 10
-column = 10
-Grid_ini = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-Grid = [[Square for a in range(column)] for a in range(row)]
+# 2. 사용자 게임 초기화 (배경화면, 게임이미지, 좌표, 폰트, 속도)
 
-# Game loop
-running = True
+
+# 이벤트 루프
+running = True  # 게임이 진행중인가?
 while running:
+    dt = clock.tick(30)
+
+    # 이벤트 처리
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.type == pygame.QUIT:  # 창 나가기 버튼을 누르면
+            running = False  # 게임이 진행중이 아님
 
-    screen.fill(RED)
+    # 3. 게임 캐릭터 위치 정의
 
-    display_grid()
+    # 4. 충돌 처리
 
-    # Update Screen
-    pygame.display.update()
+    # 5. 화면에 그리기
 
-# Quit Game
+    pygame.display.update()  # 게임 화면을 다시 그리기!
+
+# 게임 종료
 pygame.quit()
