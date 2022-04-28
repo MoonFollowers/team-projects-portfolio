1183d7e03ef2623ecd47f3bdee82c5eaa9afc363
migdracios <migdracios@gmail.com>
2022-04-28 23:36:55 +0900

    수정 - show focus 함수 일부 추가, hide focus 함수 적용시 커서가 계속 포커스 되도록 수정

--- placeholder_focus.js
+++ placeholder_focus.js
@@ -3,15 +3,39 @@
 function hide_fakeinput() {
     // 쿼리 셀렉터로 플레이스 홀더 값을 받아옴
     const fake_ph = document.querySelector('.searchbar_input').placeholder
-    const fake_ph_el = document.getElementsByClassName('searchbar_input')
-    console.log(fake_ph_el)
+    let fake_ph_el = document.getElementsByClassName('searchbar_input')
+    console.log(fake_ph, fake_ph_el)
     const fake_icon = document.querySelector('.searchbar_icon')
     const fake_span = document.querySelector('.searchbar_span')
-    if (fake_ph == '검색') {
+    if (fake_ph === '검색') {
         fake_icon.style.display = 'none'
         fake_span.style.display = 'none'
+        fake_ph.focus()
     } else {
     }
-    console.log(fake_icon)
 }
+
+// 포커스 아웃이 되거나 아이콘 버튼을 클릭하면,
+// 다시 가짜 검색 아이콘과 스팬 태그가 나오기
+
+
+// 헤더 검색 인풋 태그의 엘리멘트 가져오기
+const is_focus = document.getElementsByClassName('searchbar_input')
+// 헤더 검색 인풋 태그의 플레이스 홀더 값 가져오기
+const fake_ph = document.querySelector('.searchbar_input').placeholder
+
+// 헤더 가짜 인풋 아이템들의 값 가져오기
+const fake_icon = document.querySelector('.searchbar_icon')
+const fake_span = document.querySelector('.searchbar_span')
+
+//    포커스 아웃이라면 (이벤트 리스너)
+is_focus.addEventListener('focusout', (event) => {
+    console.log('indside eventListener')
+    // 거기에 인풋 태그가 검색이라면
+    if (fake_ph === '검색') {
+        fake_icon.style.display = 'block'
+        fake_span.style.display = 'block'
+    }
+})
+
 
