9bbca62f72384cdc07043c2898465980e37207b5
migdracios <migdracios@gmail.com>
2022-04-28 23:08:39 +0900

    주석 - hide_fakeinput 함수에 대한 설명 주석 추가

--- placeholder_focus.js
+++ placeholder_focus.js
@@ -1,6 +1,10 @@
+// 아이콘과 스팬 숨겨버리는 기능
+// 헤더 검색 플레이스 홀더 값이 '검색'이라면, 아이콘과 스팬을 숨기기
 function hide_fakeinput() {
+    // 쿼리 셀렉터로 플레이스 홀더 값을 받아옴
     const fake_ph = document.querySelector('.searchbar_input').placeholder
-    console.log(fake_ph)
+    const fake_ph_el = document.getElementsByClassName('searchbar_input')
+    console.log(fake_ph_el)
     const fake_icon = document.querySelector('.searchbar_icon')
     const fake_span = document.querySelector('.searchbar_span')
     if (fake_ph == '검색') {
@@ -10,3 +14,4 @@ function hide_fakeinput() {
     }
     console.log(fake_icon)
 }
+
