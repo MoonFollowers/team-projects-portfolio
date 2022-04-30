8519611ae5dfc6530f6e913ef949e529c74b538c
migdracios <migdracios@gmail.com>
2022-04-30 13:35:08 +0900

    시도 - 인풋박스 안에 아이콘을 넣는 방식 사용, 함수 제거 및 바닐라 JS 사용 시도

--- placeholder_focus.js
+++ placeholder_focus.js
@@ -2,20 +2,18 @@
 
 // 아이콘과 스팬 숨겨버리는 기능
 // 헤더 검색 플레이스 홀더 값이 '검색'이라면, 아이콘과 스팬을 숨기기
-function hide_fakeinput() {
+
     // 쿼리 셀렉터로 플레이스 홀더 값을 받아옴
     const fake_ph = document.querySelector('.searchbar_input').placeholder
     let fake_ph_el = document.getElementsByClassName('searchbar_input')
     console.log(fake_ph, fake_ph_el)
     const fake_icon = document.querySelector('.searchbar_icon')
-    const fake_span = document.querySelector('.searchbar_span')
     if (fake_ph === '검색') {
-        fake_icon.style.display = 'none'
-        fake_span.style.display = 'none'
+        fake_icon.style.visibility = 'hidden'
         fake_ph.focus()
     } else {
     }
-}
+
 
 
 // 포커스 아웃이 되거나 아이콘 버튼을 클릭하면,
